b5f5bc873eeba3c4d229a3cc5b290fcc848a4cfc
osaraivamatheus <osaraivamatheus@gmail.com>
2022-06-29 13:23:16 -0300

    Atualizações gerais

--- life/continuous_case.py
+++ life/continuous_case.py
@@ -12,11 +12,12 @@ class life_insurace:
     def __init__(self, qx):
         self.qx = np.array(qx)
         self.age = None
-        self.continuous_xp0_fitted = 'Not fitted yet.'
-        self.Prob_xp0 = 'Not fitted yet.'
+        self.Survival_from_birth = 'Not fitted yet.'
+        self.Survival_from_birth_Coef = 'Not estimated yet.' 
+        self.Prob_tpx = 'Not fitted yet.'
         self.Ax_integral = 'Not defined yet.'
     
-    def build_xp0(self, age):
+    def calc_tpx(self, t, x):
         """
         This is a method which produces a function that returns $Pr(T_0 > x)$, i.e, the probability of person
         aged zero survives beyound the age x given an qx distribution. Another common notation for that probability is $_xp_0$.
@@ -27,12 +28,12 @@ class life_insurace:
         lambda funtion.
         
         """
-        self.age = age
         px = 1 - self.qx
-        tpx = (lambda t: np.prod(px[:t]))
-        self.Prob_xp0 = tpx
+        tpx = np.prod(px[x:x+t])
+        return tpx
+            
     
-    def fit(self, x, y, deg):
+    def fit_Survival_from_birth(self, deg):
         """
         Given some pair of point x and y a polynomial function with degree deg is fitted. This is method to create a 
         continuous function for tpx throught polynomial interpolation method.
@@ -54,16 +55,36 @@ class life_insurace:
         
         
         """
-        x = np.array(x)
-        y = np.array(y)
-
-        f = np.polyfit(x, y, deg=deg)
-        print(Polynomial(f))
-        self.continuous_xp0_fitted = np.poly1d(f)
         
-        return self
+        vec_tpx = np.vectorize(lambda t: self.calc_tpx(t, x=0))
+        
+        t = np.arange(0, len(self.qx)+1)
+        y = vec_tpx(t=t)
+        
+        f = np.polyfit(t, y, deg=deg)
+        
+        fitted = np.poly1d(f)
+        
+        if any(fitted(t) > 1):
+            raise Exception('The function does not return a probabilistic result. Fit survival from birth again (increase degree)')
+        
+        self.Survival_from_birth_Coef = Polynomial(f)
+        self.Survival_from_birth = fitted 
+        
+        
     
-    def mu_force(self, age):
+    def Survival_from_age(self, age, to):
+        
+        if self.Survival_from_birth == 'Not fitted yet.':
+            raise Exception('You must need to fit survival from birth first')
+            
+        Sxt = self.Survival_from_birth(age+to) / self.Survival_from_birth(age)
+        
+#         if (Sxt > 1):
+#             raise Exception('The function does not return a probabilistic result. Fit survival from birth again (increase degree)')
+        return Sxt
+    
+    def mu_force(self, age, deg=5):
         
         """
         This function calculates the force of motality at some given age. This function uses the polynomial 
@@ -82,11 +103,9 @@ class life_insurace:
             The force of mortality at given age.
             
         """
+        x = np.arange(0, len(self.qx))
         
-        if self.continuous_xp0_fitted == 'Not fitted yet.':
-            raise Exception('Continuous survival function is not fitted yet.')
-        
-        mu = -1/self.continuous_xp0_fitted(age) * self.continuous_xp0_fitted.deriv()(age)
+        mu = -1/self.Survival_from_birth(age) * self.Survival_from_birth.deriv()(age)
         
         return mu
     
@@ -107,13 +126,13 @@ class life_insurace:
         
         """
         
-        if self.continuous_xp0_fitted == 'Not fitted yet.':
+        if self.Survival_from_birth == 'Not fitted yet.':
             raise Exception('Continuous survival function is not fitted yet.')
             
         if self.mu_force == 'Not fitted yet.':
             raise Exception('Mortality force function is not fitted yet.')
         
-        f = lambda time: self.mu_force(time)*np.exp(-time*delta)*self.continuous_xp0_fitted(time)
+        f = lambda time: self.mu_force(time)*np.exp(-time*delta)*self.Survival_from_birth(time)
         
         self.Ax_integral = f
     
